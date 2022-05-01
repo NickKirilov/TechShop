001181a7d120798eba66ee40282d2787e0bd7ff8
NickKirilov <nickkirilov04@gmail.com>
2022-05-01 12:48:14 +0300

    Created checkout view and functuanality

--- tech_shop_vue/src/router/index.js
+++ tech_shop_vue/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import store from "../store";
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/ProductView.vue'
@@ -8,74 +8,84 @@ import Cart from "../views/CartView.vue";
 import SignUp from "../views/SignUpView.vue";
 import LogIn from "../views/LogInView.vue";
 import MyAccount from "../views/MyAccountView.vue";
-
+import Checkout from "../views/CheckoutView.vue";
 
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/:category_slug/:product_slug/',
-    name: 'Product',
-    component: Product,
-  },
-  {
-    path: '/:category_slug',
-    name: 'Category',
-    component: Category,
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    component: Search,
-  },
-  {
-    path: '/cart',
-    name: 'Cart',
-    component: Cart,
-  },
-  {
-    path: '/sign-up',
-    name: 'SignUp',
-    component: SignUp,
-  },
-  {
-    path: '/log-in',
-    name: 'LogIn',
-    component: LogIn,
-  },
-  {
-    path: '/my-account',
-    name: 'MyAccount',
-    component: MyAccount,
-    meta: {
-      requireLogin: true,
-    }
-  },
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        component: Search,
+    },
+    {
+        path: '/cart',
+        name: 'Cart',
+        component: Cart,
+    },
+    {
+        path: '/cart/checkout',
+        name: 'Checkout',
+        component: Checkout,
+        meta: {
+            requireLogin: true,
+        }
+    },
+    {
+        path: '/:category_slug/:product_slug/',
+        name: 'Product',
+        component: Product,
+    },
+    {
+        path: '/:category_slug',
+        name: 'Category',
+        component: Category,
+    },
+
+    {
+        path: '/sign-up',
+        name: 'SignUp',
+        component: SignUp,
+    },
+    {
+        path: '/log-in',
+        name: 'LogIn',
+        component: LogIn,
+    },
+    {
+        path: '/my-account',
+        name: 'MyAccount',
+        component: MyAccount,
+        meta: {
+            requireLogin: true,
+        }
+    },
+
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  if(to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated){
-    next({name: 'LogIn', query: {to: to.path}})
-  } else {
-    next()
-  }
+    if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+        next({name: 'LogIn', query: {to: to.path}})
+    } else {
+        next()
+    }
 })
 
 export default router
